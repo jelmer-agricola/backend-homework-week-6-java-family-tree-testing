f5553bc89eb613e26042d449531b9963aafc3220
jelmer-agricola <jelmer.agricola@gmail.com>
2023-03-24 11:42:53 +0100

    first tests run

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+//        Person paul = new Person("paul", "dijxhoorn", 45, "male");
+//        String middlnamePaul = paul.getMiddleName();
+//        System.out.println(middlnamePaul);
+//        Person father = new Person("vader", "bergen", 46, "male");
+//        Person mother = new Person("moeder", "bergen", 16, "male");
+//        Person child = new Person("kind", "bergen", 12, "male");
+//
+//        child.addParents(father, mother, child);
+//        System.out.println(child.getMother().getName());
+//        System.out.println(child.getFather().getName());
+    }
+
+}
